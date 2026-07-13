d52571d3c5813a35a5ee70ed1b82b0bf1c12e8ca
araschke001-commits <araschke001@students.kusd.edu>
2026-07-12 22:09:09 -0500

    Implement MathQuill Input

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
+let mathField; // Declare mathField in the global scope
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get interface & element for MathQuill
+    let MQ = MathQuill.getInterface(2);
+    let mathFieldSpan = MQ.MathField(document.getElementById('calculatorInput'));
+    
+    // Initialize MathField
+    mathField = MQ.MathField(mathFieldSpan, {
+        handlers: {
+            edit: function() {
+                // Read input automatically on every change
+                let inputValue = mathField.latex();
+
+                detectKeywords(inputValue);
+            }
+        }
+    });
+});
+
 const buttonContainer = document.getElementById('buttons');
 const buttons = [
     'AC', '(', ')', '+',
-    '7', '8', '9', '-',
-    '4', '5', '6', '*',
-    '1', '2', '3', '/',
-    '0', '.', '<=', '='
+    '7', '8', '9', '\u2212',
+    '4', '5', '6', '\u00D7',
+    '1', '2', '3', '\u00F7',
+    '0', '.', '\u2408', '='
 ];
-
+/*
+Sqrt: u221A
+x root: u221B
+Pi: u03C0
+Power of 2: u00B2
+Power of x: u02E3
+*/
 const input = document.getElementById('calculatorInput');
 
 function CreateButtons() {
@@ -19,10 +45,14 @@ function CreateButtons() {
     });
 }
 
+function detectKeywords(inputValue) {
+    // Check for keywords in the input, like sqrt or pi, and replace them with symbols
+}
+
 function handleButtonClick(button) {
     if (button === 'AC') {
         clearAll();
-    } else if (button === '<=') {
+    } else if (button === '\u2408') {
         deleteLastCharacter();
     } else if (button === '=') {
         calculateResult(input.value);
